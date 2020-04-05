b229d56cc69081240597cbf9d39c32fd05d452db
N. Monalisa <nathaliamonalisa@gmail.com>
2020-04-05 17:32:34 -0300

    implementação da lógica de decodificação

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import cipher from './cipher.js';
+ import cipher from './cipher.js';
 
-console.log(cipher);
+ console.log(cipher);
+
+
+ //tamanho dinâmico da janela do usuário
+ let resizeScreen = () => window.resizeTo(window.innerWidth, window.innerHeight);
+
+ //Recuperar inputs do usuário
+ const GetWordUser = () => document.getElementById("encode_box").value.toUpperCase();
+
+ const GetOffsetUser = () => parseInt(document.getElementById("offset").value);
+
+ //Definir manipuladores de evento
+ const StartCoding = (event) => cipher.encode(GetOffsetUser(), GetWordUser());
+
+ const StartDecoding = (event) =>
+     cipher.decode(GetOffsetUser(), GetWordUser());
+
+ //Definir escutadores de evento
+ document.getElementById("code").addEventListener("click", StartCoding)
+ document.getElementById("decode").addEventListener("click", StartDecoding)
