e2024392c89d45eac419448af9f7ac82ebc2ef1f
N. Monalisa <nathaliamonalisa@gmail.com>
2020-04-09 23:02:36 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,61 @@
+const cipher = {
+
+    encode: function(offset, string) {
+        //variáveis de configuração
+        const alphabet_length = 26;
+        const codeASC_first_letter = 65;
+
+        //Manipulação
+        let encrypted_word = new Array();
+        for (let i = 0; i < string.length; i++) {
+            let codeASC_original = string.charCodeAt(i); //recupere o código ASC
+            if (codeASC_original !== 32) {
+                let regular_code = codeASC_original - codeASC_first_letter; //calcule o código no alfabeto 'regular' (A-Z)
+                let encrypted_letter_regular_code = (regular_code + offset) % alphabet_length //descubra a posição da letra codificada
+                let encrypted_letter_codeASC = encrypted_letter_regular_code + codeASC_first_letter; //retorne para o código ASC
+                let encrypted_letter = String.fromCharCode(encrypted_letter_codeASC).toUpperCase(); //recupere qual caracter possui esse código ASC
+                encrypted_word.push(encrypted_letter); //guarde esse caracter
+            } else {
+                encrypted_word.push(String.fromCharCode(32))
+
+            }
+        }
+        //output
+        document.getElementById("decode_box").value = (encrypted_word.join("")).toLowerCase();
+    },
+
+    decode: function(offset, string) {
+        //variáveis de configuração
+        const alphabet_length = 26;
+        const codeASC_first_letter = 65;
+
+        //Validação do campo deslocamento
+        if (isNaN(offset) === true) {
+            alert("O campo 'deslocamento' está vazio!")
+            return;
+        }
+        //Manipulação
+        let decrypted_word = new Array();
+
+        for (let i = 0; i < string.length; i++) {
+            let codeASC_original = string.charCodeAt(i);
+            if (codeASC_original !== 32) {
+                let regular_code = codeASC_original - codeASC_first_letter;
+                let decrypted_letter_regular_code = (regular_code - offset) % alphabet_length
+
+                //tratamento de posições com valores negativos)
+                if (decrypted_letter_regular_code < 0) {
+                    decrypted_letter_regular_code = parseInt(alphabet_length + decrypted_letter_regular_code)
+                }
+
+                let decrypted_letter_codeASC = decrypted_letter_regular_code + codeASC_first_letter;
+                let decrypted_letter = (String.fromCharCode(decrypted_letter_codeASC)).toLowerCase();
+                decrypted_word.push(decrypted_letter);
+            } else {
+                decrypted_word.push(String.fromCharCode(32));
+            }
+        }
+        document.getElementById("decode_box").value = (decrypted_word.join(""));
+    }
+}
+export default cipher;
---
+++ index.js
@@ -0,0 +1,58 @@
+ import cipher from './cipher.js';
+
+ //tamanho dinâmico da janela do usuário
+ window.resizeTo(window.innerWidth, window.innerHeight);
+
+ //Recuperar e validar inputs do usuário
+ const GetWordUser = () => {
+     let string = document.getElementById("encode_box").value.toUpperCase();
+     if (string !== "") {
+         return string;
+     } else {
+         alert("Insira um texto para ser codificado.");
+         throw "String is not defined"
+     }
+ };
+
+ const GetOffsetUser = () => {
+     let offset = parseInt(document.getElementById("offset").value);
+     if (isNaN(offset) !== true) {
+         return offset;
+     } else {
+         alert("O campo 'deslocamento' está vazio.");
+         throw "Offset is not defined"
+     }
+ };
+ //Definir manipuladores de evento
+ const StartCoding = () => cipher.encode(GetOffsetUser(), GetWordUser());
+
+ const StartDecoding = () => cipher.decode(GetOffsetUser(), GetWordUser());
+
+ //Intercambiar palavra entre caixas de texto
+ const ExchangeBox = () => {
+     document.getElementById("encode_box").value = document.getElementById("decode_box").value;
+     document.getElementById("decode_box").value = ""
+ };
+
+ //Reset
+ const EraseBox = () => {
+     document.getElementById("encode_box").value = "";
+     document.getElementById("decode_box").value = "";
+     document.getElementById("offset").value = "";
+ }
+
+ //Mostrar mensagem de ajuda
+ const ShowHelpMessage = () => document.querySelector("#help_balloon").style.opacity = 1;
+
+ //Fechar mensagem de ajuda
+ const CloseHelpMessage = () => document.querySelector("#help_balloon").style.opacity = 0;
+
+
+
+ //Definir escutadores de evento
+ document.getElementById("code").addEventListener("click", StartCoding)
+ document.getElementById("decode").addEventListener("click", StartDecoding)
+ document.getElementById("exchange").addEventListener("click", ExchangeBox)
+ document.getElementById("erase").addEventListener("click", EraseBox)
+ document.getElementById("help_btn").addEventListener("click", ShowHelpMessage)
+ document.getElementById("close_popUp").addEventListener("click", CloseHelpMessage)
