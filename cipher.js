30ff888dc846c54b37c75b1832593c89bce1e2ab
N. Monalisa <nathaliamonalisa@gmail.com>
2020-04-10 20:44:24 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,61 +1,83 @@
+//variáveis de configuração
+const alphabet_length = 26;
+const codeASC_first_letter = 65;
+const codeAsc_space = 32;
+
 const cipher = {
-
+    //início de cipher.encode()
     encode: function(offset, string) {
-        //variáveis de configuração
-        const alphabet_length = 26;
-        const codeASC_first_letter = 65;
 
-        //Manipulação
-        let encrypted_word = new Array();
+        //Validação dos dados
+        //campos vazios
+        if (isNaN(offset) == true || string == "") {
+            throw new TypeError("Os campos 'deslocamento' e 'mensagem' não podem estar vazios.");
+            //offset nulo
+        } else if (offset == 0) {
+            throw new TypeError("O argumento 'deslocamento' deve ser maior que 0.");
+            //mensagem não é do tipo string
+        } else if (typeof string !== "string") {
+            throw new TypeError('A mensagem deve ser um texto.');
+        } else {
+            //há números como caracteres no meio da mensagem
+            for (let i = 0; i < string.length; i++) {
+                if (string.charAt(i) == "0" ||
+                    string.charAt(i) == "1" ||
+                    string.charAt(i) == "2" ||
+                    string.charAt(i) == "3" ||
+                    string.charAt(i) == "4" ||
+                    string.charAt(i) == "5" ||
+                    string.charAt(i) == "6" ||
+                    string.charAt(i) == "7" ||
+                    string.charAt(i) == "8" ||
+                    string.charAt(i) == "9") {
+                    throw new TypeError("A mensagem não pode conter números.");
+                }
+            }
+        }
+
+        //Codificação
+        let encrypted_message = "";
         for (let i = 0; i < string.length; i++) {
             let codeASC_original = string.charCodeAt(i); //recupere o código ASC
-            if (codeASC_original !== 32) {
+
+            if (codeASC_original !== codeAsc_space) {
                 let regular_code = codeASC_original - codeASC_first_letter; //calcule o código no alfabeto 'regular' (A-Z)
-                let encrypted_letter_regular_code = (regular_code + offset) % alphabet_length //descubra a posição da letra codificada
+                let encrypted_letter_regular_code = (regular_code + offset) % alphabet_length; //descubra a posição da letra codificada
                 let encrypted_letter_codeASC = encrypted_letter_regular_code + codeASC_first_letter; //retorne para o código ASC
-                let encrypted_letter = String.fromCharCode(encrypted_letter_codeASC).toUpperCase(); //recupere qual caracter possui esse código ASC
-                encrypted_word.push(encrypted_letter); //guarde esse caracter
+                let encrypted_letter = String.fromCharCode(encrypted_letter_codeASC); //recupere qual caracter possui esse código ASC
+                encrypted_message += encrypted_letter; //guarde esse caracter
             } else {
-                encrypted_word.push(String.fromCharCode(32))
-
+                encrypted_message += String.fromCharCode(codeASC_original);
             }
         }
-        //output
-        document.getElementById("decode_box").value = (encrypted_word.join("")).toLowerCase();
+        return encrypted_message;
     },
+    //fim de cipher.encode()
 
+    //inicio de cipher.decode()
     decode: function(offset, string) {
-        //variáveis de configuração
-        const alphabet_length = 26;
-        const codeASC_first_letter = 65;
+            let decrypted_message = "";
 
-        //Validação do campo deslocamento
-        if (isNaN(offset) === true) {
-            alert("O campo 'deslocamento' está vazio!")
-            return;
-        }
-        //Manipulação
-        let decrypted_word = new Array();
+            for (let i = 0; i < string.length; i++) {
+                let codeASC_original = string.charCodeAt(i);
 
-        for (let i = 0; i < string.length; i++) {
-            let codeASC_original = string.charCodeAt(i);
-            if (codeASC_original !== 32) {
-                let regular_code = codeASC_original - codeASC_first_letter;
-                let decrypted_letter_regular_code = (regular_code - offset) % alphabet_length
+                if (codeASC_original !== codeAsc_space) {
+                    let regular_code = codeASC_original - codeASC_first_letter;
+                    let decrypted_letter_regular_code = (regular_code - offset) % alphabet_length;
 
-                //tratamento de posições com valores negativos)
-                if (decrypted_letter_regular_code < 0) {
-                    decrypted_letter_regular_code = parseInt(alphabet_length + decrypted_letter_regular_code)
+                    //tratamento de posições com valores negativos)
+                    if (decrypted_letter_regular_code < 0) {
+                        decrypted_letter_regular_code = parseInt(alphabet_length + decrypted_letter_regular_code);
+                    }
+
+                    let decrypted_letter_codeASC = decrypted_letter_regular_code + codeASC_first_letter;
+                    let decrypted_letter = (String.fromCharCode(decrypted_letter_codeASC));
+                    decrypted_message += decrypted_letter;
+                } else {
+                    decrypted_message += String.fromCharCode(codeASC_original);
                 }
-
-                let decrypted_letter_codeASC = decrypted_letter_regular_code + codeASC_first_letter;
-                let decrypted_letter = (String.fromCharCode(decrypted_letter_codeASC)).toLowerCase();
-                decrypted_word.push(decrypted_letter);
-            } else {
-                decrypted_word.push(String.fromCharCode(32));
             }
-        }
-        document.getElementById("decode_box").value = (decrypted_word.join(""));
-    }
+            return decrypted_message;
+        } //fim de cipher.decode()   
 }
 export default cipher;
